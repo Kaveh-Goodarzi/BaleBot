2c8f1b85d4efeda935f3a1018dd9d3c573b84ee3
Kaveh Goodarzi <kaveh4.voll@gmail.com>
2026-02-03 13:23:47 +0330

    Update file

--- config/config.go
+++ config/config.go
@@ -1,5 +1,5 @@
 package config
 
 const BotToken = "387702848:Bo6vfopU1D2r5nU-j44WIxGY1ZRNffqjDUs"
-const WebhookURL = "https://balebot-d7mz.onrender.com"
+const WebhookURL = "https://balebot-d7mz.onrender.com/webhook"
 const DBFile = "bot.db"
